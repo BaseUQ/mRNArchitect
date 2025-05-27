39856916bb94407bba28ec7f60a67c0ec4ab4720
Jonathon Waterhouse <jon@wtrhs.com>
2025-05-27 14:01:42 +1000

    Performance improvements (#3)
    * improve performance of python script
    
    * refactor python cli for performance
    
    * return None if empty sequence
    
    * fix tsc errors
    
    * wip
    
    * build dev for test
    
    * add routeTree
    
    * remove log
    
    * use gha cache

--- app.config.ts
+++ app.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
   routers: {
     client: {
       vite: {
+        // @ts-ignore
         server: {
           hmr: {
             port: 8081,
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,4 @@
+// @ts-ignore
 import mantineCssUrl from "@mantine/core/styles.css?url";
 
 import {
