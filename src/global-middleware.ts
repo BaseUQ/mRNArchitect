4ca162b5cc2465f972685b74b97eb367a14e3a04
Jonathon Waterhouse <jon@wtrhs.com>
2025-06-10 11:00:24 +1000

    log IP address

--- src/global-middleware.ts
+++ src/global-middleware.ts
@@ -2,17 +2,20 @@ import {
   createMiddleware,
   registerGlobalMiddleware,
 } from "@tanstack/react-start";
+import { getHeaders } from "@tanstack/react-start/server";
 import { differenceInMilliseconds } from "date-fns";
 
 const loggingMiddleware = createMiddleware().server(async ({ next, data }) => {
   const startTime = Date.now();
   const result = await next();
   const endTime = Date.now();
+  const headers = getHeaders();
   try {
     console.log(
       JSON.stringify({
         request: data,
         response: result,
+        ip: headers["x-forwarded-for"] ?? "unknown",
         timeMs: differenceInMilliseconds(endTime, startTime),
       }),
     );
