0a0870039b54ca552e6a5e2ef0d6c2544c8c1a34
Jonathon Waterhouse <jon@wtrhs.com>
2025-08-07 13:39:19 +1000

    [feat] add five prime UTR select (#16)
    [feat] add five prime UTR select

--- src/components/pages/optimize/Help.tsx
+++ src/components/pages/optimize/Help.tsx
@@ -69,8 +69,7 @@ export const Help = () => {
               ],
               [
                 "5' UTR",
-                "Paste your 5' untranslated sequence here. The 5' untranslated region (UTR) is bound and scanned by the ribosome and is needed for translation. We provide a well-validated option, the human alpha-globin (HBA1; Gene ID 3039) 5' UTR sequence that has been validated in different cell types and applications. By default, no 5' UTR will be added.",
-                /*<Fragment key="five-prime-utr">
+                <Fragment key="five-prime-utr">
                   Paste your 5' untranslated sequence here. The 5' untranslated
                   region (UTR) is bound and scanned by the ribosome and is
                   needed for translation. We provide well-validated 5' UTRs from
@@ -79,7 +78,7 @@ export const Help = () => {
                   ID ENSG00000075624) and albumin (ALB; Gene ID
                   ENSG00000163631), and a minimal 5' UTR<sup>2</sup>. By
                   default, no 5' UTR will be added.
-                </Fragment>,*/
+                </Fragment>,
               ],
               [
                 "3' UTR",
