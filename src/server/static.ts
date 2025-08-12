3c91e145bc1549ff0a9c961e01e425237ec3c904
Jonathon Waterhouse <jon@wtrhs.com>
2025-08-12 14:45:10 +1000

    [bug] fix docker build

--- src/server/static.ts
+++ src/server/static.ts
@@ -4,6 +4,6 @@ import { readFileAsync } from "./utils";
 export const loadTermsAndConditions = createServerFn({
   type: "static",
 }).handler(async () => {
-  const data = await readFileAsync("./TERMS_AND_CONDITIONS.md");
+  const data = await readFileAsync("../../TERMS_AND_CONDITIONS.md");
   return data.toString();
 });
