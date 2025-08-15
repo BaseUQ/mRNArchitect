e96d3e89e553549893fe21c3b175d157ce3bcba9
Jonathon Waterhouse <jon@wtrhs.com>
2025-08-15 11:33:23 +1000

    only keep versions in open prs

--- .github/scripts/prune-lambda-versions.py
+++ .github/scripts/prune-lambda-versions.py
@@ -1,22 +1,41 @@
 # /// script
 # dependencies = [
-#   "boto3"
+#   "boto3",
+#   "PyGithub"
 # ]
 # ///
 
-
+import argparse
 import datetime
+import os
 
 import boto3
+import github
 
 FUNCTION_NAME = "app"
 
 ALIASES_TO_KEEP = ["PRODUCTION", "TEST"]
 
-KEEP_IF_WITHIN = datetime.timedelta(weeks=2)
+# KEEP_IF_WITHIN = datetime.timedelta(weeks=2)
 
 if __name__ == "__main__":
-    """Prunes unused lambda versions/aliases."""
+    parser = argparse.ArgumentParser(
+        description="Prune lambda versions/aliases that are no longer in use."
+    )
+    parser.add_argument("repo", type="str", help="The GitHub repository name.")
+    args = parser.parse_args()
+
+    github_token = os.getenv("GH_TOKEN")
+    if not github_token:
+        raise RuntimeError(
+            "Github token not available. Ensure the the GH_TOKEN env var is set."
+        )
+
+    github_client = github.Client(auth=github.Auth(github_token))
+    repo = github_client.get_repo(args.repo)
+    open_pull_requests = repo.get_pulls(state="open")
+    open_pull_request_numbers = [str(pr.number) for pr in open_pull_requests]
+
     lambda_client = boto3.client("lambda")
 
     versions = lambda_client.list_versions_by_function(FunctionName=FUNCTION_NAME)[
@@ -36,20 +55,23 @@ if __name__ == "__main__":
         version_aliases = [
             a for a in aliases if a["FunctionVersion"] == version["Version"]
         ]
-        if any(a["Name"] in ALIASES_TO_KEEP for a in version_aliases):
+        if any(
+            a["Name"] in open_pull_request_numbers or a["Name"] in ALIASES_TO_KEEP
+            for a in version_aliases
+        ):
             print(
                 f"Keeping version {version['Version']} with aliases {[a['Name'] for a in version_aliases]}."
             )
             continue
 
-        version_last_modified = datetime.datetime.fromisoformat(version["LastModified"])
-        if version_last_modified + KEEP_IF_WITHIN > datetime.datetime.now(
-            tz=datetime.UTC
-        ):
-            print(
-                f"Keeping version {version['Version']} with LastModified {version['LastModified']}."
-            )
-            continue
+        # version_last_modified = datetime.datetime.fromisoformat(version["LastModified"])
+        # if version_last_modified + KEEP_IF_WITHIN > datetime.datetime.now(
+        #    tz=datetime.UTC
+        # ):
+        #    print(
+        #        f"Keeping version {version['Version']} with LastModified {version['LastModified']}."
+        #    )
+        #    continue
 
         print(
             f"Deleting version {version['Version']} and aliases {[a['Name'] for a in version_aliases]}."
