4985cddb00d034dbe680189e7312788d56faacdd
Jonathon Waterhouse <jon@wtrhs.com>
2025-08-21 13:11:57 +1000

    [feat] show warning if missing stop codon (#48)

--- src/components/pages/optimize/ProgressLoader.tsx
+++ src/components/pages/optimize/ProgressLoader.tsx
@@ -1,15 +1,16 @@
-import { Center, Loader, Stack, Text } from "@mantine/core";
+import { Card, Center, Container, Loader, Stack, Text } from "@mantine/core";
 import { useInterval } from "@mantine/hooks";
 import { formatDuration, intervalToDuration } from "date-fns";
-import { useState } from "react";
+import { PropsWithChildren, useState } from "react";
 
 interface ProgressLoaderProps {
   estimatedTimeInSeconds: number;
 }
 
 export const ProgressLoader = ({
+  children,
   estimatedTimeInSeconds,
-}: ProgressLoaderProps) => {
+}: ProgressLoaderProps & PropsWithChildren) => {
   const [elapsedSeconds, setElapsedSeconds] = useState<number>(0);
   const _elapsedInterval = useInterval(
     () => setElapsedSeconds((s) => s + 1),
@@ -27,13 +28,18 @@ export const ProgressLoader = ({
   );
 
   return (
-    <Center p="lg">
-      <Stack align="center">
-        <Loader type="dots" />
-        <Text>Optimisation in progress...</Text>
-        <Text size="sm">{`Estimated time: < ${formattedEstimatedTime}`}</Text>
-        <Text size="sm">{`Elapsed time: ${formattedElapsedTime}`}</Text>
-      </Stack>
-    </Center>
+    <Container size="sm">
+      <Center p="lg">
+        <Card shadow="sm" radius="md" withBorder>
+          <Stack align="center">
+            <Loader type="dots" />
+            <Text>Optimisation in progress...</Text>
+            <Text size="sm">{`Estimated time: < ${formattedEstimatedTime}`}</Text>
+            <Text size="sm">{`Elapsed time: ${formattedElapsedTime}`}</Text>
+            {children}
+          </Stack>
+        </Card>
+      </Center>
+    </Container>
   );
 };
