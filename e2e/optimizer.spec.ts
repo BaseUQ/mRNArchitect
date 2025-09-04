05b3684cf3691af086018079473c1e3b7365ac9d
Jonathon Waterhouse <jon@wtrhs.com>
2025-09-04 12:55:30 +1000

    [feat] add codon usage metrics (#54)
    * [feat] add codon usage metrics
    
    * fix
    
    * fix test

--- e2e/optimizer.spec.ts
+++ e2e/optimizer.spec.ts
@@ -1,44 +1,21 @@
 import { expect, test } from "@playwright/test";
 
-const _EGFP_AMINO_ACID_SEQUENCE =
-  "MVSKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTLVTTLTYGVQCFSRYPDHMKQHDFFKSAMPEGYVQERTIFFKDDGNYKTRAEVKFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYNSHNVYIMADKQKNGIKVNFKIRHNIEDGSVQLADHYQQNTPIGDGPVLLPDNHYLSTQSALSKDPNEKRDHMVLLEFVTAAGITLGMDELYK";
 const EGFP_NUCLEIC_ACID_SEQUENCE =
   "ATGGTGAGCAAGGGCGAGGAGCTGTTCACCGGCGTGGTGCCCATCCTGGTGGAGCTGGACGGCGACGTGAACGGCCACAAGTTCAGCGTGAGCGGCGAGGGAGAGGGCGACGCCACCTATGGCAAGCTGACCCTGAAGTTCATCTGCACCACCGGCAAGCTGCCCGTGCCCTGGCCCACACTGGTGACCACCCTGACCTACGGCGTGCAGTGCTTCAGCAGATACCCCGACCACATGAAGCAGCACGATTTCTTCAAGAGCGCCATGCCCGAGGGCTACGTGCAGGAGAGAACCATCTTCTTCAAGGACGACGGCAACTACAAGACCAGAGCCGAGGTGAAGTTCGAGGGCGACACCCTGGTGAACAGAATCGAGCTGAAGGGCATCGACTTCAAGGAGGATGGCAACATCCTGGGCCACAAGCTGGAGTACAACTACAACAGCCACAACGTGTACATCATGGCCGACAAGCAGAAGAACGGCATCAAGGTGAACTTCAAGATCAGACACAACATCGAGGACGGCAGCGTGCAGCTGGCCGACCACTACCAGCAGAACACCCCCATCGGCGACGGCCCCGTGCTGCTGCCCGACAACCACTACCTGAGCACCCAGAGCGCCCTGAGCAAGGACCCCAACGAGAAGAGAGACCACATGGTGCTGCTGGAGTTCGTGACCGCCGCCGGCATCACCCTGGGCATGGACGAGCTGTACAAG";
 
-//test("run optimization - eGFP amino acid", async ({ page }) => {
-//  await page.goto("/");
-//  await page.waitForTimeout(1_000); // brief wait for the form to load, should make this better
-//  await page.getByRole("radio", { name: "Amino acid" }).dispatchEvent("click");
-//  await page
-//    .getByRole("textbox", { name: "Coding sequence textarea" })
-//    .fill(EGFP_AMINO_ACID_SEQUENCE);
-//  await page.getByRole("button", { name: "Optimise sequence" }).click();
-//
-//  await expect(page.getByRole("tab", { name: "Output" })).toHaveAttribute(
-//    "aria-selected",
-//    "true",
-//    { timeout: 30_000 },
-//  );
-//  await expect(page.getByText(EGFP_NUCLEIC_ACID_SEQUENCE)).toHaveCount(1);
-//});
+test("run optimization - pre-fill random example", async ({ page }) => {
+  await page.goto("/");
+  await page.waitForTimeout(1_000); // brief wait for the form to load, should make this better
+  await page.getByRole("button", { name: "Pre-fill random example" }).click();
+  await expect(page.getByLabel("Coding sequence textarea")).not.toBeEmpty();
+  await page.getByRole("button", { name: "Optimise sequence" }).click();
 
-//test("run optimization - eGFP amino acid (pre-fill example)", async ({
-//  page,
-//}) => {
-//  await page.goto("/");
-//  await page.waitForTimeout(1_000); // brief wait for the form to load, should make this better
-//  await page
-//    .getByRole("button", { name: "Pre-fill example sequence (eGFP)" })
-//    .click();
-//  await page.getByRole("button", { name: "Optimise sequence" }).click();
-//
-//  await expect(page.getByRole("tab", { name: "Output" })).toHaveAttribute(
-//    "aria-selected",
-//    "true",
-//    { timeout: 30_000 },
-//  );
-//  await expect(page.getByText(EGFP_NUCLEIC_ACID_SEQUENCE)).toHaveCount(1);
-//});
+  await expect(page.getByRole("tab", { name: "Output" })).toHaveAttribute(
+    "aria-selected",
+    "true",
+    { timeout: 30_000 },
+  );
+});
 
 test("run optimization - eGFP nucleic acid", async ({ page }) => {
   await page.goto("/");
--- tools/constants.py
+++ tools/constants.py
@@ -2,78 +2,42 @@ import typing
 
 from .types import Codon, AminoAcid
 
+AMINO_ACIDS = set(typing.get_args(AminoAcid))
+
 
 CODONS = set(typing.get_args(Codon))
 
 
-AMINO_ACIDS = set(typing.get_args(AminoAcid))
+AMINO_ACID_TO_CODONS_MAP: dict[AminoAcid, set[Codon]] = {
+    "R": {"AGA", "AGG", "CGA", "CGC", "CGG", "CGT"},
+    "K": {"AAA", "AAG"},
+    "C": {"TGC", "TGT"},
+    "A": {"GCA", "GCC", "GCG", "GCT"},
+    "V": {"GTA", "GTC", "GTG", "GTT"},
+    "Y": {"TAC", "TAT"},
+    "G": {"GGA", "GGC", "GGG", "GGT"},
+    "*": {"TAA", "TAG", "TGA"},
+    "L": {"CTA", "CTC", "CTG", "CTT", "TTA", "TTG"},
+    "Q": {"CAA", "CAG"},
+    "M": {"ATG"},
+    "H": {"CAC", "CAT"},
+    "T": {"ACA", "ACC", "ACG", "ACT"},
+    "I": {"ATA", "ATC", "ATT"},
+    "W": {"TGG"},
+    "N": {"AAC", "AAT"},
+    "S": {"AGC", "AGT", "TCA", "TCC", "TCG", "TCT"},
+    "F": {"TTC", "TTT"},
+    "P": {"CCA", "CCC", "CCG", "CCT"},
+    "D": {"GAC", "GAT"},
+    "E": {"GAA", "GAG"},
+}
+"""Maps an amino acid 1-letter symbol to a set of codons."""
 
 
 CODON_TO_AMINO_ACID_MAP: dict[Codon, AminoAcid] = {
-    "TAA": "*",
-    "TAG": "*",
-    "TGA": "*",
-    "GCA": "A",
-    "GCC": "A",
-    "GCG": "A",
-    "GCT": "A",
-    "TGC": "C",
-    "TGT": "C",
-    "GAC": "D",
-    "GAT": "D",
-    "GAA": "E",
-    "GAG": "E",
-    "TTC": "F",
-    "TTT": "F",
-    "GGA": "G",
-    "GGC": "G",
-    "GGG": "G",
-    "GGT": "G",
-    "CAC": "H",
-    "CAT": "H",
-    "ATA": "I",
-    "ATC": "I",
-    "ATT": "I",
-    "AAA": "K",
-    "AAG": "K",
-    "CTA": "L",
-    "CTC": "L",
-    "CTG": "L",
-    "CTT": "L",
-    "TTA": "L",
-    "TTG": "L",
-    "ATG": "M",
-    "AAC": "N",
-    "AAT": "N",
-    "CCA": "P",
-    "CCC": "P",
-    "CCG": "P",
-    "CCT": "P",
-    "CAA": "Q",
-    "CAG": "Q",
-    "AGA": "R",
-    "AGG": "R",
-    "CGA": "R",
-    "CGC": "R",
-    "CGG": "R",
-    "CGT": "R",
-    "AGC": "S",
-    "AGT": "S",
-    "TCA": "S",
-    "TCC": "S",
-    "TCG": "S",
-    "TCT": "S",
-    "ACA": "T",
-    "ACC": "T",
-    "ACG": "T",
-    "ACT": "T",
-    "GTA": "V",
-    "GTC": "V",
-    "GTG": "V",
-    "GTT": "V",
-    "TGG": "W",
-    "TAC": "Y",
-    "TAT": "Y",
+    codon: amino_acid
+    for amino_acid, codons in AMINO_ACID_TO_CODONS_MAP.items()
+    for codon in codons
 }
 """Maps a codon to an amino acid 1-letter symbol."""
 
