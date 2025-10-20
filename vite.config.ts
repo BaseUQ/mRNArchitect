5dfde24fd648c8d155a3de9679163b068689959d
Jonathon Waterhouse <jon@wtrhs.com>
2025-10-20 15:53:34 +1000

    [fix] use nitro to run server (#73)
    * [fix] use nitro to run server
    
    * fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   server: {
     hmr: {
       port: 8081,
@@ -23,10 +24,11 @@ export default defineConfig({
       },
       srcDirectory: "src",
     }),
+    mode === "production" ? nitro() : null, // Using the nitro plugin breaks vitest, so only enable it in production
     viteReact(),
   ],
   test: {
     include: ["src/**/*.test.{ts,tsx}"],
     exclude: ["e2e/**"],
   },
-});
+}));
