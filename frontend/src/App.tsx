5745cfd0217c1d723f32ccbc8cfbc22288f61c72
Jonathon Waterhouse <jon@wtrhs.com>
2025-10-30 21:42:26 +1000

    [bug] fix bundle size

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import { AppShell, Container, MantineProvider } from "@mantine/core";
-import { OptimizePage } from "~/components/pages/optimize/OptimizePage";
+import { lazy, Suspense } from "react";
+
+const OptimizePage = lazy(() =>
+  import("~/components/pages/optimize/OptimizePage").then((module) => ({
+    default: module.OptimizePage,
+  })),
+);
 
 const App = () => (
   <MantineProvider>
     <AppShell padding="md">
       <AppShell.Main>
         <Container size="md">
-          <OptimizePage />
+          <Suspense fallback={<div>Loading...</div>}>
+            <OptimizePage />
+          </Suspense>
         </Container>
       </AppShell.Main>
     </AppShell>
