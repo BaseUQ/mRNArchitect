59d7ffb719eaad7dd8ccef806ebcdc7098d90e8a
Jonathon Waterhouse <jon@wtrhs.com>
2025-12-17 11:07:18 +1000

    [chore] redirect direction production URL to base website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { AppShell, Container, MantineProvider } from "@mantine/core";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
 
 const OptimizePage = lazy(() =>
   import("~/components/pages/optimize/OptimizePage").then((module) => ({
@@ -7,8 +7,27 @@ const OptimizePage = lazy(() =>
   })),
 );
 
-const App = () => (
-  <MantineProvider>
+const App = () => {
+  useEffect(() => {
+    /**
+      * NOTE: This is to prevent people from accessing the app directly
+      * at https://app.basefacility.org.au.
+      * If it is running under this domain (and not in an iframe), then
+      * we redirect them to https://basefacility.org.au where the app
+      * will load correctly in the iframe and the user must login to access it.
+      */
+    // Check if NOT running in an iframe.
+    if (window.self === window.top) {
+      // Check if the host is the production URL.
+      if (window.location.hostname === "app.basefacility.org.au") {
+        // Redirect to the basefacility.org.au/software page.
+        window.location.href = "https://basefacility.org.au/software";
+      }
+    }
+  }, []);
+
+  return (
+    <MantineProvider>
     <AppShell padding="md">
       <AppShell.Main>
         <Container size="md">
@@ -19,6 +38,7 @@ const App = () => (
       </AppShell.Main>
     </AppShell>
   </MantineProvider>
-);
+  );
+};
 
 export default App;
